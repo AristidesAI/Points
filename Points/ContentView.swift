845d47cb951ff01d883a0d7e18e7dbdc3c5484a6
Aristides Lintzeris <aristideslintzeris@icloud.com>
2026-07-02 22:49:07 +1000

    Initial Commit

---
+++ Points/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Points
+//
+//  Created by Ari on 2/7/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Points/PointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointsApp.swift
+//  Points
+//
+//  Created by Ari on 2/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct PointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
